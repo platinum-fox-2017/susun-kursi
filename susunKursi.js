1210d5fc4447e4e6748d8310bf572b42188c615d
agungcaproex <agung.caproex@gmail.com>
2018-02-06 11:50:01 +0700

    upload susunKursi

--- susunKursi.js
+++ susunKursi.js
@@ -9,21 +9,21 @@
 // Baris 2 :  [ 'c', 'f', 'i' ]
 
 'use strict'
-
 function generateSeats(row) {
+
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<row; i++) {
+		seats.push([])
 	}
 	return seats
 }
-
+debugger
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
+	for(let i=0; i<arr.length; i++) {
+		seats[i % 3].push(arr[i])
 		if(row <= 2) {
 			row++
 		} else {
@@ -34,7 +34,7 @@ function managePerson(arr, rowSeats) {
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i=0; i<seats.length; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
